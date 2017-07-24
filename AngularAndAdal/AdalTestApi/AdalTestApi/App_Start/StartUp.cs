5a0561d7338f9a13d36f10fcf9995c0dcb0a9e69
bosjaH <jorenpaps@gmail.com>
2017-07-24 23:26:57 +0200

    AngularAndAdal: files I forgot

---
+++ AngularAndAdal/AdalTestApi/AdalTestApi/App_Start/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AdalTestApi.App_Start.Startup))]
+namespace AdalTestApi.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            HttpConfiguration config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+            new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+            {
+                Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidAudience = ConfigurationManager.AppSettings["ida:Audience"]
+                },
+            });
+        }
+    }
+}
---
+++ AngularAndAdal/AdalTestApi/AdalTestApi/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AdalTestApi.Controllers
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Function { get; set; }
+    }
+
+    [Authorize]
+    public class EmployeeController : ApiController
+    {
+        private static readonly Employee[] SpikesEmployees = new Employee[] {
+            new Employee() { Id = 1, Name = "Maarten", Function = "Team lead" },
+            new Employee() { Id = 2, Name = "Bart", Function = "BI Consultant" },
+            new Employee() { Id = 3, Name = "Joren", Function = "Developer" },
+            new Employee() { Id = 4, Name = "Ruben", Function = "Developer" },
+            new Employee() { Id = 5, Name = "Thomas", Function = "O365 Developer" },
+            new Employee() { Id = 6, Name = "Koen", Function = "O365 Developer" },
+            new Employee() { Id = 7, Name = "Tom", Function = "O365 Architect" },
+            new Employee() { Id = 8, Name = "Jurgen", Function = "Infra" },
+            new Employee() { Id = 9, Name = "Jasper", Function = "Infra" },
+            new Employee() { Id = 10, Name = "Dirk", Function = "Sales" },
+            new Employee() { Id = 11, Name = "Marc", Function = "Boss" },
+            new Employee() { Id = 12, Name = "Inge", Function = "HR" },
+        };
+
+        public IEnumerable<Employee> Get()
+        {
+            return SpikesEmployees;
+        }
+    }
+}
